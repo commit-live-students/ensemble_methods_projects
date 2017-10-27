93169aeaa0323e6ffdf0ecdb3484f59535271bd6
rameshgayam <ramesh.gayam@gmail.com>
2017-10-27 18:45:27 +0000

    Done

--- q02_stacking_clf/build.py
+++ q02_stacking_clf/build.py
@@ -1,9 +1,11 @@
+# %load q02_stacking_clf/build.py
 # Default imports
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import BaggingClassifier
 from sklearn.metrics import accuracy_score
+from mlxtend.classifier import StackingClassifier
 import pandas as pd
 import numpy as np
 
@@ -16,5 +18,11 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 
 # Write your code here
 
+def stacking_clf(X_train, X_test, y_train, y_test,model):
+    stacking_clf = StackingClassifier(classifiers = model,
+                                 meta_classifier = DecisionTreeClassifier())
 
-
+    stacking_clf.fit(X_train, y_train)
+    y_pred = stacking_clf.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    return accuracy
