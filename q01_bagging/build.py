3da8e22eacefa15e6c3555708b0a4eaa4cee0216
chiragpujari <chg.p786@gmail.com>
2018-03-15 09:45:33 +0000

    Done

--- q01_bagging/build.py
+++ q01_bagging/build.py
@@ -12,7 +12,21 @@ X = dataframe.iloc[:, :-1]
 y = dataframe.iloc[:, -1]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=9)
 
-
+r1=[]
+r2=[]
 # Write your code here
+def bagging(X_train,X_test,y_train,y_test,n_est):
+    for est in range(1,n_est):
+        bagging_clf2 = BaggingClassifier(DecisionTreeClassifier(), n_estimators=est,max_samples=0.67, max_features=0.67,
+                                bootstrap=True, random_state=9)
 
+        bagging_clf2.fit(X_train, y_train)
+        y_pred_tr = bagging_clf2.predict(X_train)
+        y_pred_ts = bagging_clf2.predict(X_test)
 
+        r1.append(accuracy_score(y_train, y_pred_tr))
+        r2.append(accuracy_score(y_test, y_pred_ts))
+
+    plt.plot(range(1,n_est),r1)
+    plt.plot(range(1,n_est),r2)
+    plt.show()
