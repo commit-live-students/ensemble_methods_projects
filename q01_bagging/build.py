6a3f33e3ce9fd293f68f09719d5cfd1ce468a31e
AdityaMV1215 <aditya.vadhavkar12@gmail.com>
2018-04-17 17:31:43 +0000

    Done

--- q01_bagging/build.py
+++ q01_bagging/build.py
@@ -2,6 +2,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import BaggingClassifier
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score
 
@@ -14,5 +16,19 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 
 
 # Write your code here
+def bagging(X_train, X_test, y_train, y_test, n_est):
+    model = DecisionTreeClassifier(random_state=9)
+    bag = BaggingClassifier(base_estimator=model, n_estimators=n_est)
+    bag.fit(X_train, y_train)
+    y_pred = bag.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    return accuracy
 
+acc = []
+x = []
+for i in range(1,51):
+    x.append(i)
+    acc.append(bagging(X_train, X_test, y_train, y_test, i))
 
+plt.plot(x=x, y=acc)
+plt.show()
