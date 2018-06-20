ec9c03b50482cb0081c4fa34e0a46f646a3fbdfe
sonal7930 <sonal.chandra7930@gmail.com>
2018-06-20 14:20:45 +0000

    Done

--- q01_bagging/build.py
+++ q01_bagging/build.py
@@ -2,6 +2,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import BaggingClassifier
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score
 
@@ -14,5 +16,25 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 
 
 # Write your code here
+values = list(range(1,50))
+def bagging(X_train, X_test, y_train, y_test,n_est):
+    final=[]
+    def bagging_subset(n_est):
+        bagging_clf1 = BaggingClassifier(DecisionTreeClassifier(), n_estimators=n_est, max_samples=0.67,max_features=0.67,
+                                bootstrap=True, random_state=9)
+        bagging_clf1.fit(X_train, y_train)
+        y_pred_bagtest = bagging_clf1.predict(X_test)
+        score_test = accuracy_score(y_test, y_pred_bagtest)
 
+        y_pred_bagtrain = bagging_clf1.predict(X_train)
+        score_train = accuracy_score(y_train, y_pred_bagtrain)
+        #print n_est1, score_test, score_train
+        return (n_est, score_test, score_train)
 
+    results = map(lambda n_est :  bagging_subset(n_est), values)
+    results = pd.DataFrame(results)
+
+    plt.plot(results[:][0],results[:][1])
+    plt.plot(results[:][0],results[:][2])
+    plt.show()
+    return
