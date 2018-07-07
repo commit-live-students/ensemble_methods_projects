7aa3d102157845e6625920d36a0f35d7410c1250
sonal7930 <sonal.chandra7930@gmail.com>
2018-07-07 20:09:37 +0000

    Done

--- q01_bagging/build.py
+++ q01_bagging/build.py
@@ -1,9 +1,8 @@
+# %load q01_bagging/build.py
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import BaggingClassifier
-import matplotlib
-matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score
 
@@ -16,25 +15,28 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 
 
 # Write your code here
-values = list(range(1,50))
-def bagging(X_train, X_test, y_train, y_test,n_est):
-    final=[]
-    def bagging_subset(n_est):
-        bagging_clf1 = BaggingClassifier(DecisionTreeClassifier(), n_estimators=n_est, max_samples=0.67,max_features=0.67,
+def bagging(X_train, X_test, y_train, y_test,n_est = 10):
+    decision_clf = DecisionTreeClassifier()
+    
+    # Fitting single decision tree
+    decision_clf.fit(X_train, y_train)
+    y_pred_decision = decision_clf.predict(X_test)
+    score_dt1 = accuracy_score(y_test, y_pred_decision)
+    
+    
+    # Fitting bagging classifier with DecisionTreeClassifier
+    bagging_clf1 = BaggingClassifier(decision_clf, n_est, max_samples=0.67,max_features=0.67, 
                                 bootstrap=True, random_state=9)
-        bagging_clf1.fit(X_train, y_train)
-        y_pred_bagtest = bagging_clf1.predict(X_test)
-        score_test = accuracy_score(y_test, y_pred_bagtest)
 
-        y_pred_bagtrain = bagging_clf1.predict(X_train)
-        score_train = accuracy_score(y_train, y_pred_bagtrain)
-        #print n_est1, score_test, score_train
-        return (n_est, score_test, score_train)
+    bagging_clf1.fit(X_train, y_train)
+    y_pred_bagging = bagging_clf1.predict(X_test)
+    score_bc_dt = accuracy_score(y_test, y_pred_bagging)
+    
+    return plt.plot(n_est,score_bc_dt)
+    
 
-    results = map(lambda n_est :  bagging_subset(n_est), values)
-    results = pd.DataFrame(results)
 
-    plt.plot(results[:][0],results[:][1])
-    plt.plot(results[:][0],results[:][2])
-    plt.show()
-    return
+
+print(bagging(X_train, X_test, y_train, y_test,n_est = 10))
+plt.show()
+
