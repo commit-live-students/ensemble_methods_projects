4809e5afe451520c3ec92e7e5e4b6b0a17266f7c
sannidh <soniknitr@gmail.com>
2018-12-11 08:41:44 +0000

    Done

--- q02_stacking_clf/build.py
+++ q02_stacking_clf/build.py
@@ -1,3 +1,4 @@
+# %load q02_stacking_clf/build.py
 # Default imports
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
@@ -7,12 +8,35 @@ from sklearn.metrics import accuracy_score
 import pandas as pd
 import numpy as np
 
-# Loading data
 dataframe = pd.read_csv('data/loan_prediction.csv')
 X = dataframe.iloc[:, :-1]
 y = dataframe.iloc[:, -1]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=9)
 
-# Write your code here
+clf1 = LogisticRegression(random_state=9)
+clf2 = DecisionTreeClassifier(random_state=9)
+clf3 = DecisionTreeClassifier(max_depth=9, random_state=9)
+
+bagging_clf1 = BaggingClassifier(clf2, n_estimators=100, max_samples=100,
+                                 bootstrap=True, random_state=9, oob_score=True)
+bagging_clf2 = BaggingClassifier(clf1, n_estimators=100, max_samples=100,
+                                 bootstrap=True, random_state=9, oob_score=True)
+bagging_clf3 = BaggingClassifier(clf3, n_estimators=100, max_samples=100,
+                                 bootstrap=True, random_state=9, oob_score=True)
+model = [bagging_clf1, bagging_clf2, bagging_clf3]
+def stacking_clf(model, X_train, y_train, X_test, y_test):
+    y_pred_train, y_pred_test=[], []
+    for ind_model in model:
+        ind_model.fit(X_train, y_train)
+        y_pred_train.extend(ind_model.predict(X_train))
+        y_pred_test.extend(ind_model.predict(X_test))
+    X_test_new =  X_test.append(X_test.append(X_test))
+    X_train_new =  X_train.append(X_train.append(X_train))
+    clf1.fit(X_train_new, y_pred_train)
+    y_pred_test_metaclf = clf1.predict(X_test)
+    return accuracy_score(y_test, y_pred_test_metaclf)
+
+
+
 
